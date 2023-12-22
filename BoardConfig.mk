cb315ce017013a2c8e3b6ec7b51035bee23ac70d
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-22 14:09:48 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6833B
+DEVICE_PATH := device/infinix/X6833B
 
 # Inherit from mt6789-common
 include device/transsion/mt6789-common/BoardConfigCommon.mk
@@ -17,4 +17,4 @@ TARGET_INIT_VENDOR_LIB := libinit_Infinix-X6833B
 TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X6833B
 
 # TWRP Configs
-TW_DEVICE_VERSION := X6833B_by_rama982
+TW_DEVICE_VERSION := rama982-Turkishpatriot
