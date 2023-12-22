02862b38e33ddd40f6d5ead61ff53812bf8ec362
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-22 14:10:15 +0300

    Update twrp_X6833B.mk

--- twrp_X6833B.mk
+++ twrp_X6833B.mk
@@ -5,14 +5,14 @@
 #
 
 # Inherit from Infinix-X6833B device
-$(call inherit-product, device/infinix/Infinix-X6833B/device.mk)
+$(call inherit-product, device/infinix/X6833B/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
 PRODUCT_NAME := twrp_X6833B
-PRODUCT_DEVICE := Infinix-X6833B
+PRODUCT_DEVICE := X6833B
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6833B
 PRODUCT_MANUFACTURER := INFINIX
